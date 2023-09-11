e3765cfaf7f472fe72f8af217480658327986b63
Dennis Miring'u <dennismiringu03@gmail.com>
2023-09-10 23:39:11 -0700

    All

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-int i;
+	int i;
 
-for (i = 0; i < 10; i++)
-{
-putchar(i + '0');
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return (0);
 }
 
-putchar('\n');
-
-return (0);
