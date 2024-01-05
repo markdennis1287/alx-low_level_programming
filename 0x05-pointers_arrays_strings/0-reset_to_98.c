80cedb54747848762723210bc5c275a68b3a0903
Dennis Miringu <dennismiringu03@gmail.com>
2024-01-04 21:44:02 -0800

    all

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value pointed to by n to 98
+ * @n: pointer to integer to be updated
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+
+void print_rev(char *s)
+{
+int length = 0;
+
+while (*s != '\0')
+{
+length++;
+s++;
+}
+
+while (length > 0)
+{
+s--;
+_putchar(*s);
+length--;
+}
+
+_putchar('\n');
+}
